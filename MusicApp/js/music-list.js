0c25df02363eb6b8f81352adcca73efdec5058db
Vaishnavi <155608394+S-M-Vaishnavi@users.noreply.github.com>
2024-02-06 12:14:03 +0530

    Add files via upload

---
+++ MusicApp/js/music-list.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+    {
+      name: "Jimmiki Ponnu",
+      artist: "Anirudh",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Ranjithamey",
+      artist: "Anirudh",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Soul of Varisu",
+      artist: "Anirudh",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Sita Raman Theme",
+      artist: "AR Rahaman",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+];
